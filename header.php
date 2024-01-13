873779712d6f3112a2e4e6ac064ef0748d200309
“fleur3028” <156543068+fleur3028@users.noreply.github.com>
2024-01-13 22:52:28 +0100

    h

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {session_start();}
+
+if(isset($_SESSION['login'])){
+    $id = $_SESSION['login'];
+}
+
+else {
+    $id = Null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require "header.php";?>
+<div class="container mx-auto pr-3 mt-3 mb-4 bg-secondary">
+
+
+</div>
+<?php require "footer.php";?>
